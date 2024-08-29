98e4382ec40713e1f16b0a99efe09b30a8d5d72d
Sebastian Poblete <spobleteobando@gmail.com>
2024-08-28 23:35:50 -0400

    FIX js

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@
 */
 //
 // Scripts
-// 
+//
 
 window.addEventListener('DOMContentLoaded', event => {
 
@@ -20,7 +20,6 @@ window.addEventListener('DOMContentLoaded', event => {
         } else {
             navbarCollapsible.classList.add('navbar-shrink')
         }
-
     };
 
     // Shrink the navbar 
@@ -51,24 +50,25 @@ window.addEventListener('DOMContentLoaded', event => {
         });
     });
 
-    //Cerrar videos de modales
-            // Escuchar cuando el DOM esté completamente cargado
-            document.addEventListener('DOMContentLoaded', function () {
-                // Seleccionar todos los modales
-                var modales = document.querySelectorAll('.portfolio-modal');
-                
-                modales.forEach(function (modal) {
-                    // Añadir un listener al evento de cierre del modal
-                    modal.addEventListener('hidden.bs.modal', function () {
-                        // Seleccionar todos los iframes dentro del modal
-                        var iframes = modal.querySelectorAll('iframe');
-                        
-                        // Resetear el src de cada iframe para detener los videos
-                        iframes.forEach(function (iframe) {
-                            iframe.src = iframe.src;
-                        });
-                    });
+    // Función para detener videos de YouTube en los modales al cerrarlos
+    const stopYouTubeVideos = () => {
+        const modales = document.querySelectorAll('.portfolio-modal');
+
+        modales.forEach(modal => {
+            // Añadir un listener al evento de cierre del modal
+            modal.addEventListener('hidden.bs.modal', () => {
+                // Seleccionar todos los iframes dentro del modal
+                const iframes = modal.querySelectorAll('iframe');
+
+                // Resetear el src de cada iframe para detener los videos
+                iframes.forEach(iframe => {
+                    iframe.src = iframe.src;
                 });
             });
+        });
+    };
+
+    // Llamar a la función para detener videos
+    stopYouTubeVideos();
 
 });
